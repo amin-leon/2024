7dc0b9fa35aa4caa85a86e0f06b2a8f877d492fb
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2024-05-27 14:15:14 +0200

    fixing logics

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ const loginUser = async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid credentials.' });
+      return res.status(401).json({ error: 'Invalid Password.' });
     }
 
     if (user.approvalStatus === 'pending' || user.accountStatus === 'inactive') {
@@ -121,7 +121,7 @@ const loginUser = async (req, res) => {
     return res.json({ message: 'Login successful.', token });
 
   } catch (error) {
-    console.error('Login failed:', error);
+    console.log('Login failed:', error);
     return res.status(500).json({ error: 'Could not login.' });
   }
 };
